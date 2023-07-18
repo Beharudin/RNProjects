b6f184f4ccccd25533bad3c028f1dd8c5c003068
Beharudin <beharudinmm@gmail.com>
2023-07-18 09:15:24 +0300

    Added AwesomeProject app for the first time.

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: "Bahar", age: 25 },
+    { name: "Abdi", age: 23 },
+    { name: "Caalaa", age: 15 },
+    { name: "Umar", age: 27 },
+    { name: "Bini", age: 45 },
+    { name: "Temu", age: 34 },
+    { name: "Usmaan", age: 22 },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        {people.map((person) => (
+          <View style={styles.item} key={person.age}>
+            <Text>
+              {person.name} {person.age}
+            </Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +38,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginHorizontal: 5,
+    marginTop: 25,
+  },
+  item: {
+    fontSize: 25,
+    marginTop: 25,
+    backgroundColor: "pink",
+    margin: 30,
+    padding: 50,
   },
 });
