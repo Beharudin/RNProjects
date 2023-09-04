4addde3e94bb188f431426488b6bd0bd357e081a
Beharudin <beharudinmm@gmail.com>
2023-09-04 09:48:46 +0300

    Some changes

--- App.js
+++ App.js
@@ -1,29 +1,9 @@
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
 
-
-function App() {
-
+const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-      </View>
-    </SafeAreaView>
+    <AppNavContainer></AppNavContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  }
-});
+};
 
 export default App;
---
+++ src/navigations/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const AppNavContainer = () => {
+  return (
+    <NavigationContainer>
+      <Text>Hello World1</Text>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavContainer;
