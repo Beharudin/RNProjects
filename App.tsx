7bd41f07270930bb1935e6c51898dc4586c48378
Beharudin <beharudinmm@gmail.com>
2023-09-04 09:00:55 +0300

    Added RNContacts

---
+++ App.tsx
@@ -0,0 +1,30 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  }
+});
+
+export default App;
