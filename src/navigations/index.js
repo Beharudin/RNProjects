c324f87d02c42fbc552b1d3bc5fcb2e968003273
Beharudin <beharudinmm@gmail.com>
2023-09-04 16:02:29 +0300

    some changes

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
-import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import HomeNavigator from './HomeNavigator';
 import AuthNavigator from './AuthNavigator';
 import DrawerNavigator from './DrawerNavigator';
 
